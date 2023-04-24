f7e711c890aee79e8671e626f8d67259b3303877
hock <tehboonhock@hotmail.com>
2023-04-25 00:57:57 +0800

    test commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'gaworkflow'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gaworkflow');
+    expect(app.title).toEqual('gaworkflow test');
   });
 
   it('should render title', () => {
